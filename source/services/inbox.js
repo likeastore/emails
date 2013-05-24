e9901ecf63594611434d6b94c1eb99ac7d8f5c2d
alexanderbeletsky <alexander.beletsky@gmail.com>
2013-05-24 20:56:00 +0300

    messenger

---
+++ source/services/inbox.js
@@ -0,0 +1,31 @@
+var db = require('./../db/dbConnector').db;
+
+function inbox (messenger) {
+	return {
+		all: function (mailbox, callback) {
+			db.emails.find({mailbox: mailbox}, function (err, emails) {
+				if (err) {
+					return callback(err);
+				}
+
+				messenger.trigger('emails:read', emails);
+				callback(null, emails);
+			});
+		},
+
+		post: function (mailbox, email, callback) {
+			email.mailbox = mailbox;
+
+			db.emails.insert(email, function (err, email) {
+				if (err) {
+					return callback(err);
+				}
+
+				messenger.trigger('emails:posted', email);
+				callback(null, email);
+			});
+		}
+	};
+}
+
+module.exports = inbox;
