4d6e0f0059765f416300ceac9c9eb3d17efa7a8c
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-08-05 18:17:46 +0300

    aug announce

--- source/api.js
+++ source/api.js
@@ -1,9 +1,15 @@
 var sendCollectionPromo = require('./services/collectionsPromo/promo-w31');
+var sendAnnouce = require('./services/sendAugustAnnounce');
 
 function api(app) {
 
 	app.post('/api/send/collPromo', function (req, res) {
 		sendCollectionPromo(function () {});
+		res.send(201);
+	});
+
+	app.post('/api/send/announce', function (req, res) {
+		sendAnnouce(function () {});
 		res.send(201);
 	});
 }
